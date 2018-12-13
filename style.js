d764f4187917dfa4b5c841bf2470ff4c8cdbb214
ArtemSmotritel <43967754+ArtemSmotritel@users.noreply.github.com>
2018-12-14 00:22:23 +0200

    Create style.js

---
+++ style.js
@@ -0,0 +1,53 @@
+main {
+	display: block;
+}
+
+body {
+	font-family: 'Pacifico';
+	/*align-self: center;*/
+	width: 90%;
+	margin-left: 5%;
+	margin-right: 5%;
+	padding: 0;
+    border: 0;
+    background-color: black;
+    background-image: url(sky.png); 
+    color: white;
+}
+
+header	{
+	align-self: center;
+	align-items: center; 
+}
+
+h1 {
+	text-align: center;
+	width: 100%;
+	margin-left: auto;
+	font-size: 70px;
+  	font-weight: 600;
+  	color: yellow;
+}
+
+.main {
+	font-size: 22px;
+  	font-weight: 250;
+}
+
+.description {
+	color: #ffffcc;
+	font-size: 30px;
+  	font-weight: 300;
+}
+
+footer {
+	position: fixed; 
+    left: 5%; bottom: 3%; 
+}
+
+.copyright {
+	font-size: 15px;
+  	font-weight: 200;
+	margin-left: 0px;
+	color: #ffffcc;
+}
